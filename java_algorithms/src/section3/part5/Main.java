c405a48f1da9e4e2a8316db8cd255905c4e69d75
syh8088 <syh8088@gmail.com>
2024-08-27 11:04:53 +0900

    5. 소수(에라토스테네스 체)
    5. 소수(에라토스테네스 체)

---
+++ java_algorithms/src/section3/part5/Main.java
@@ -0,0 +1,35 @@
+package section3.part5;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int n) {
+
+        int answer = 0;
+
+        int [] ch = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            if (ch[i] == 0) {
+                answer++;
+
+                for (int j = i; j <= n; j = j + i) {
+                    ch[j] = 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+//        int n = kb.nextInt();
+//        System.out.print(T.solution(n));
+
+        System.out.print(T.solution(20));
+    }
+}
