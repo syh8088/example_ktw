01fe4a792f2f361cfabd6afd0225cdd072774d19
syh8088 <syh8088@gmail.com>
2024-08-28 20:56:05 +0900

    4. 연속부분수열(복합적 문제)
    4. 연속부분수열(복합적 문제)

--- java_algorithms/src/section4/part2/Main.java
+++ java_algorithms/src/section4/part2/Main.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int solution2(int n, int k, int[] arr) {
-
-        int answer = 0;
-
-
+    public ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
 
         return answer;
     }
@@ -19,63 +16,22 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
 
-//        int n = kb.nextInt();
-//        int k = kb.nextInt();
-//        int[] arr = new int[n];
-//
-//        for (int i = 0; i < n; i++) {
-//            arr[i] = kb.nextInt();
-//        }
-//
-//        System.out.print(T.solution3(n, k, arr));
-
-        System.out.print(T.solution3(
-                10,
-                3,
-                new int[]{12, 15, 11, 20, 25, 10, 20, 19, 13, 15}
-        ));
+        for (int x : T.solution2(
+                5,
+                5,
+                new int[]{3, 2, 5, 7, 8}
+        )) {
+            System.out.print(x + " ");
+        }
 
     }
 
-    public int solution3(int n, int k, int[] arr) {
-        int answer = 0;
-        int sum = 0;
+    public ArrayList<Integer> solution2(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
 
-        for (int i = 0; i < k; i++) {
-            sum += arr[i];
-        }
-        answer = sum;
-        for (int i = k; i < n; i++) {
-            sum += arr[i] - arr[i - k];
-            answer = Math.max(answer, sum);
-        }
-
+        int p1 = 0, p2 = 0;
 
         return answer;
     }
 
-    /**
-     * 내가 풀어본거
-     */
-    public int solution(int n, int k, int[] arr) {
-
-        int answer = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            int i1 = arr[i];
-            if (n - k > i) {
-                for (int j = i + 1; j < i + k; j++) {
-                    int j1 = arr[j];
-                    System.out.println("i = " + i + " j1 = " + j1);
-                    i1 += j1;
-                }
-
-                answer = Math.max(answer, i1);
-            }
-        }
-
-
-        return answer;
-    }
 }
