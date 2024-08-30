bfb509e435a8e831956c116a532e2a8fa09ecbf3
syh8088 <syh8088@gmail.com>
2024-08-30 12:11:49 +0900

    4. 후위식 연산(postfix)
    4. 후위식 연산(postfix)

---
+++ java_algorithms/src/section6/part4/Main.java
@@ -0,0 +1,60 @@
+package section6.part4;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public int solution2(String str) {
+
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+//        String str = kb.next();
+//        System.out.print(T.solution(str));
+
+        System.out.print(T.solution("352+*9-"));
+    }
+
+    public int solution(String str) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+
+            char c = charArray[i];
+            if (Character.isDigit(c)) { // 숫자
+                stack.push(c - 48);
+            }
+            else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+
+                if (c == '+') {
+                    stack.push(lt + rt);
+                }
+                else if (c == '-') {
+                    stack.push(lt - rt);
+                }
+                else if (c == '*') {
+                    stack.push(lt * rt);
+                }
+                else if (c == '/') {
+                    stack.push(lt / rt);
+                }
+            }
+
+        }
+
+        return stack.get(0);
+    }
+
+}
