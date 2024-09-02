1cddac0daf69a22d0ddf5a859a5a4424788f8e5e
syh8088 <syh8088@gmail.com>
2024-09-02 16:48:48 +0900

    12. 경로탐색(DFS)
    12. 경로탐색(DFS)

---
+++ java_algorithms/src/section8/part12/Main.java
@@ -0,0 +1,74 @@
+package section8.part12;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int answer = 0;
+    static int[][] graph;
+    static int[] ch;
+
+    public void BFS2(int v) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+//        n = kb.nextInt();
+//        m = kb.nextInt();
+        n = 5;
+        m = 9;
+
+//        graph = new int[n + 1][n + 1];
+        ch = new int[n + 1];
+
+
+//        for (int i = 0; i < m; i++) {
+//            int a = kb.nextInt();
+//            int b = kb.nextInt();
+//            graph[a][b] = 1;
+//        }
+
+        graph = new int[][]{
+                {1, 2},
+                {1, 3},
+                {1, 4},
+                {2, 1},
+                {2, 3},
+                {2, 5},
+                {3, 4},
+                {4, 2},
+                {4, 5}
+        };
+
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
+
+    }
+
+    public int DFS(int v) {
+
+        if (v == n) answer++;
+        else {
+            for (int i = 1; i <= n; i++) {
+                if (graph[v][i] == 1 && ch[i] == 0) {
+                    ch[i] = 1;
+                    DFS(i);
+                    ch[i] = 0;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
