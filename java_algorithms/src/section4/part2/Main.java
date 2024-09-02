4198f82f50e8adac87fa34086eb0192cd220bcdb
syh8088 <syh8088@gmail.com>
2024-09-02 17:12:34 +0900

    2. 공통원소구하기(two pointers algorithm) - 다시 풀어보기
    2. 공통원소구하기(two pointers algorithm) - 다시 풀어보기

--- java_algorithms/src/section4/part2/Main.java
+++ java_algorithms/src/section4/part2/Main.java
@@ -1,12 +1,14 @@
 package section4.part2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    public ArrayList<Integer> solution(int n, int k, int[] arr) {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
         ArrayList<Integer> answer = new ArrayList<>();
+        
 
         return answer;
     }
@@ -16,9 +18,10 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
 
-        for (int x : T.solution2(
+        for (int x : T.solution(
                 5,
                 5,
+                new int[]{1, 3, 9, 5, 2},
                 new int[]{3, 2, 5, 7, 8}
         )) {
             System.out.print(x + " ");
