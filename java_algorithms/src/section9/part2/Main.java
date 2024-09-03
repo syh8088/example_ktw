d58b2baad937c25f94b51b2286da6162e9c14bce
syh8088 <syh8088@gmail.com>
2024-09-03 14:06:19 +0900

    2. 바둑이 승차
    2. 바둑이 승차

---
+++ java_algorithms/src/section9/part2/Main.java
@@ -0,0 +1,50 @@
+package section9.part2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int answer = Integer.MIN_VALUE;
+    static int n;
+    static int c;
+
+    public void DFS2(int L, int sum, int[] arr) {
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+//        c = kb.nextInt();
+//        n = kb.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = kb.nextInt();
+//        }
+//          T.DFS(0, 0, arr);
+//        System.out.print(answer);
+
+        n = 5;
+        c = 259;
+        int[] arr = new int[] {81, 58, 42, 33, 61};
+        T.DFS(0, 0, arr);
+        System.out.print(answer);
+    }
+
+    public void DFS(int L, int sum, int[] arr) {
+
+        if (sum > c) return;
+
+        if (L == n) {
+            answer = Math.max(sum, answer);
+        }
+        else {
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
+    }
+
+}
