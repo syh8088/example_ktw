b59b46d265a1ed7dcdf74f0bd373214cb295e475
syh8088 <syh8088@gmail.com>
2024-09-04 14:54:13 +0900

    2. 회의실 배정
    2. 회의실 배정

---
+++ java_algorithms/src/section10/part2/Main.java
@@ -0,0 +1,82 @@
+package section10.part2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class CourseSlug implements Comparable<CourseSlug> {
+
+    public int start;
+    public int end;
+
+    public CourseSlug(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(CourseSlug o) {
+        if (this.end == o.end) {
+            return this.start - o.start;
+        }
+        else {
+            return this.end - o.end;
+        }
+    }
+}
+
+public class Main {
+
+    public int solution2(int n, ArrayList<CourseSlug> arr) {
+        int cnt = 0;
+
+
+
+        return cnt;
+    }
+
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+//        int n = kb.nextInt();
+
+        ArrayList<CourseSlug> arr = new ArrayList<>();
+
+//        for (int i = 0; i < n; i++) {
+//            int start = kb.nextInt();
+//            int end = kb.nextInt();
+//            arr.add(new Body(start, end));
+//        }
+
+        arr.add(new CourseSlug(1, 4));
+        arr.add(new CourseSlug(2, 3));
+        arr.add(new CourseSlug(3, 5));
+        arr.add(new CourseSlug(4, 6));
+        arr.add(new CourseSlug(5, 7));
+
+        System.out.println(T.solution(5, arr));
+    }
+
+    /**
+     *
+     */
+    public int solution(int n, ArrayList<CourseSlug> arr) {
+
+        int cnt = 0;
+
+        Collections.sort(arr);
+
+        int end = 0;
+        for (int i = 0; i < n; i++) {
+            CourseSlug target = arr.get(i);
+
+            if (end <= target.start) {
+                end = target.end;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
