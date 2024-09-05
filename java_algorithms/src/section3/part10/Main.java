f749c2bc81bbfaafad80a46c03f8733f536d4231
syh8088 <syh8088@gmail.com>
2024-09-05 22:48:10 +0900

    10. 봉우리 - 다시 풀어보기
    10. 봉우리 - 다시 풀어보기

--- java_algorithms/src/section3/part10/Main.java
+++ java_algorithms/src/section3/part10/Main.java
@@ -7,6 +7,38 @@ public class Main {
     int[] dx = {-1, 0, 1, 0};
     int[] dy = {0, 1, 0, -1};
 
+    public int solution2(int n, int[][] arr) {
+
+        int answer = 0;
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+//        int n = kb.nextInt();
+//        int[][] arr = new int[n][n];
+//
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                arr[i][j] = kb.nextInt();
+//            }
+//        }
+
+        System.out.print(T.solution2(5, new int[][] {
+                { 5, 3, 7, 2, 3 },
+                { 3, 7, 1, 6, 1 },
+                { 7, 2, 5, 3, 4 },
+                { 4, 3, 6, 4, 1 },
+                { 8, 7, 3, 5, 2 }
+        }));
+
+    }
+
     public int solution(int n, int[][] target) {
 
         int answer = 0;
@@ -72,29 +104,5 @@ public class Main {
 
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-
-//        int n = kb.nextInt();
-//        int[][] arr = new int[n][n];
-//
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                arr[i][j] = kb.nextInt();
-//            }
-//        }
-
-        System.out.print(T.solution(5, new int[][] {
-                { 5, 3, 7, 2, 3 },
-                { 3, 7, 1, 6, 1 },
-                { 7, 2, 5, 3, 4 },
-                { 4, 3, 6, 4, 1 },
-                { 8, 7, 3, 5, 2 }
-        }));
-
     }
 }
