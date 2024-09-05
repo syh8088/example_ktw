0b6912ad53d6bb93f5932c062248b424eb865cf9
syh8088 <syh8088@gmail.com>
2024-09-05 20:48:08 +0900

    8. 등수구하기 - 다시 풀어보기
    8. 등수구하기 - 다시 풀어보기

--- java_algorithms/src/section3/part8/Main.java
+++ java_algorithms/src/section3/part8/Main.java
@@ -1,10 +1,39 @@
 package section3.part8;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
+
+    public int[] solution2(int n, int[] arr) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+//        int n = kb.nextInt();
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = kb.nextInt();
+//        }
+//
+//        for (int result : T.solution(n, arr)) {
+//            System.out.println(result);
+//        }
+
+        for (int result : T.solution2(
+                5,
+//                new int[] {87, 89, 92, 100, 76}
+                new int[] {87, 87, 92, 100, 76}
+        )
+        ) {
+            System.out.println(result);
+        }
+    }
 
     public int[] solution(int n, int[] target) {
 
@@ -29,23 +58,4 @@ public class Main {
         return answer;
     }
 
-    public static void main(String[] args) {
-
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-
-//        int n = kb.nextInt();
-//        int[] arr = new int[n];
-//        for (int i = 0; i < n; i++) {
-//            arr[i] = kb.nextInt();
-//        }
-//
-//        for (int result : T.solution(n, arr)) {
-//            System.out.println(result);
-//        }
-
-        for (int result : T.solution(5, new int[] {87, 87, 92, 100, 76})) {
-            System.out.println(result);
-        }
-    }
 }
