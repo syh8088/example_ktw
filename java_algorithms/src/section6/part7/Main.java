3909bba57cb7096bb0fb602f9970b6f4e7a844c7
syh8088 <syh8088@gmail.com>
2024-09-05 16:35:07 +0900

    7. 교육과정설계 - 다시 풀어보기
    7. 교육과정설계 - 다시 풀어보기

--- java_algorithms/src/section6/part7/Main.java
+++ java_algorithms/src/section6/part7/Main.java
@@ -1,12 +1,13 @@
 package section6.part7;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
-    public String solution2(String a, String b) {
+    public String solution4(String a, String b) {
 
         String answer = "YES";
 
@@ -19,15 +20,45 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
 
-//        String a = kb.next();
-//        String b = kb.next();
-//        System.out.print(T.solution(a, b));
+        String a = kb.next();
+        String b = kb.next();
+        System.out.print(T.solution(a, b));
 
-        System.out.print(T.solution("CBA", "CBDAGE"));
+//        System.out.print(T.solution2("CBA", "CBDAGE"));
     }
 
     /**
-     *
+     * 내가 풀어본거
+     */
+    public String solution2(String a, String b) {
+
+        String answer = "YES";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        char[] charArray = a.toCharArray();
+        for (char c : charArray) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        char[] charArray1 = b.toCharArray();
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : charArray1) {
+            if (map.containsKey(c)) {
+                queue.offer(c);
+            }
+        }
+
+        String test = "";
+        while (!queue.isEmpty()) {
+            Character c = queue.poll();
+            test += c;
+        }
+
+        return (test.equals(a)) ? "YES" : "NO";
+    }
+
+    /**
+     * 문제 해설용
      */
     public String solution(String a, String b) {
 
