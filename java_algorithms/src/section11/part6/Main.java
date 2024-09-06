a3b96ad612ff5cec9096de3290ba0e31bbb4eca8
syh8088 <syh8088@gmail.com>
2024-09-06 15:05:15 +0900

    11. 임시반장정하기 - 다시 풀어보기
    11. 임시반장정하기 - 다시 풀어보기

---
+++ java_algorithms/src/section11/part6/Main.java
@@ -0,0 +1,45 @@
+package section11.part6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution2(int n, int[] arr, int m) {
+
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int dy[] = new int[m + 1];
+
+        for (int i = 1; i < n; i++) {
+            int ps = kb.nextInt();
+            int pt = kb.nextInt();
+            for (int j = m; j >= pt; j--) {
+                dy[j] = Math.max(dy[j], dy[j - pt] + ps);
+            }
+        }
+
+        System.out.println(dy[m]);
+    }
+
+    public int solution(int n, int m) {
+//
+//        int[] dy = new int[m + 1];
+//        for (int i = 0; i < n; i++) {
+//            int ps = kb
+//        }
+
+        return 0;
+    }
+}
