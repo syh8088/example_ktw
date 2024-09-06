31c6faa3cb7637cff1cb139e75071f165f740a04
syh8088 <syh8088@gmail.com>
2024-09-06 17:14:00 +0900

    12. 멘토링
    12. 멘토링

--- java_algorithms/src/section3/part12/Main.java
+++ java_algorithms/src/section3/part12/Main.java
@@ -8,6 +8,18 @@ public class Main {
 
         int answer = 0;
 
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
+                int target = arr[i][j];
+                System.out.println("target = " + target);
+                for (int k = 0; k < col; k++) {
+                    int i1 = arr[j][k];
+                    System.out.println("i1 = " + i1);
+                }
+            }
+        }
+
         return answer;
     }
 
@@ -27,7 +39,7 @@ public class Main {
 //        }
 //        System.out.print(T.solution(n, m, arr));
 
-        System.out.print(T.solution2(4, 3, new int[][] {
+        System.out.print(T.solution3(4, 3, new int[][] {
                 { 3, 4, 1, 2 },
                 { 4, 3, 2, 1 },
                 { 3, 1, 4, 2 }
@@ -67,29 +79,4 @@ public class Main {
         return answer;
     }
 
-    public int solution(int col, int row, int[][] arr) {
-
-        int answer = 0;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-
-                int target = arr[i][j];
-                for (int k = j; k < col; k++) {
-
-                    int memti = arr[i][k];
-                    System.out.println("memti = " + memti);
-                    System.out.println("target = " + target);
-                    if (target != memti) {
-
-                    }
-
-
-
-                }
-            }
-        }
-
-        return answer;
-    }
 }
