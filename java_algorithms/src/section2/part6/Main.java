c5e0be6f8dc9a52cba90752621e53646ada9328d
syh8088 <syh8088@gmail.com>
2024-09-10 17:15:31 +0900

    6. 중복문자제거 - 다시 풀어보기
    6. 중복문자제거 - 다시 풀어보기

--- java_algorithms/src/section2/part6/Main.java
+++ java_algorithms/src/section2/part6/Main.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
+    public String solution2(String str) {
+
+        String answer = "";
+
+
+        
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+//        String str = kb.next();
+        String str = "ksekkset";
+        T.solution2(str);
+    }
+
+
     public String solution(String str) {
 
         String answer = "";
@@ -17,13 +38,5 @@ public class Main {
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        String str = kb.next();
-        T.solution(str);
     }
 }
