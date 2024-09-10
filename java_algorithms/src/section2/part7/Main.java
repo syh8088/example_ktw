b4048d989da3970df001817b3222b7209ca7c9df
syh8088 <syh8088@gmail.com>
2024-09-10 15:51:00 +0900

    7. 회문문자열 - 다시 풀어보기
    7. 회문문자열 - 다시 풀어보기

--- java_algorithms/src/section2/part7/Main.java
+++ java_algorithms/src/section2/part7/Main.java
@@ -4,6 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
+    public String solution2(String str) {
+
+        String answer = "YES";
+
+
+
+
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+//        String str = kb.next();
+        String str = "gooG";
+        System.out.print(T.solution2(str));
+    }
+
+
     public String solution(String str) {
 
         String answer = "YES";
@@ -26,13 +48,5 @@ public class Main {
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        String str = kb.next();
-        System.out.print(T.solution(str));
     }
 }
