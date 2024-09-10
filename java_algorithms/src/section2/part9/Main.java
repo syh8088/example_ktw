b7ec1c23eec3f0f5098c40d49ff4e0a42b9ee7d4
syh8088 <syh8088@gmail.com>
2024-09-10 18:10:40 +0900

    9. 숫자만 추출 - 다시 풀어보기
    9. 숫자만 추출 - 다시 풀어보기

--- java_algorithms/src/section2/part9/Main.java
+++ java_algorithms/src/section2/part9/Main.java
@@ -4,12 +4,50 @@ import java.util.Scanner;
 
 public class Main {
 
+    public int solution3(String str) {
+
+        String answer = "";
+
+
+
+
+
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+//        String str = kb.next();
+        String str = "g0en2T0s8eSoft";
+        System.out.print(T.solution(str));
+    }
+
+
     public int solution(String str) {
+
+        int answer = 0;
+
+        for (char c : str.toCharArray()) {
+
+            if (c >= 48 && c <= 57) {
+                answer = answer * 10 + (c - 48);
+            }
+        }
+
+        return answer;
+    }
+
+    public int solution2(String str) {
 
         String answer = "";
 
         for (char c : str.toCharArray()) {
-            if (Character.isAlphabetic(c)) {
+
+            if (Character.isDigit(c)) {
                 answer += c;
             }
         }
@@ -17,11 +55,4 @@ public class Main {
         return Integer.parseInt(answer);
     }
 
-    public static void main(String[] args) {
-
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        String str = kb.next();
-        System.out.print(T.solution(str));
-    }
 }
