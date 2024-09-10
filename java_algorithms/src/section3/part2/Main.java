3be9b9a0473da66819a09c123778fcaacda10127
syh8088 <syh8088@gmail.com>
2024-09-10 21:52:40 +0900

    2. 보이는 학생 - 다시 풀어보기
    2. 보이는 학생 - 다시 풀어보기

--- java_algorithms/src/section3/part2/Main.java
+++ java_algorithms/src/section3/part2/Main.java
@@ -5,6 +5,37 @@ import java.util.Scanner;
 
 public class Main {
 
+    public int solution2(int n, int[] arr) {
+
+        int answer = 0;
+        int pre = 0;
+        for (int i = 0; i < n; i++) {
+            int target = arr[i];
+            if (pre < target) {
+                answer++;
+                pre = target;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print(T.solution2(8, new int[] {130, 135, 148, 140, 145, 150, 150, 153}));
+
+//        int n = kb.nextInt();
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = kb.nextInt();
+//        }
+//        System.out.print( T.solution2(n, arr));
+    }
+
+
     public int solution(int n, int[] arr) {
 
         ArrayList<Integer> answer = new ArrayList<>();
@@ -26,19 +57,21 @@ public class Main {
         return answer.size();
     }
 
-    public static void main(String[] args) {
+    /**
+     * 내가 풀어본거...
+     */
+    public int solution3(int n, int[] arr) {
 
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
+        int answer = 0;
+        int pre = 0;
+        for (int i = 0; i < n; i++) {
+            int target = arr[i];
+            if (pre < target) {
+                answer++;
+                pre = target;
+            }
+        }
 
-        System.out.print(T.solution(8, new int[] {130, 135, 148, 140, 145, 150, 150, 153}));
-
-//        int n = kb.nextInt();
-//        int[] arr = new int[n];
-//        for (int i = 0; i < n; i++) {
-//            arr[i] = kb.nextInt();
-//        }
-//
-//        System.out.print( T.solution(n, arr));
+        return answer;
     }
 }
