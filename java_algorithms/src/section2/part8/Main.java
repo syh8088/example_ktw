2652ebc670b529626ed804297b40eec18bb0a199
syh8088 <syh8088@gmail.com>
2024-09-10 17:43:22 +0900

    8. 팰린드롬(replaceAll 정규식이용) - 다시 풀어보기
    8. 팰린드롬(replaceAll 정규식이용) - 다시 풀어보기

--- java_algorithms/src/section2/part8/Main.java
+++ java_algorithms/src/section2/part8/Main.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 
 public class Main {
 
+    public String solution2(String str) {
+
+        String answer = "NO";
+
+
+
+
+
+        
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+//        String str = kb.next();
+        String str = "found7, time: study; Yduts; emit, 7Dnuof";
+        System.out.print(T.solution2(str));
+    }
+
+
+
     public String solution(String str) {
 
         String answer = "NO";
@@ -16,13 +39,5 @@ public class Main {
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        String str = kb.next();
-        System.out.print(T.solution(str));
     }
 }
