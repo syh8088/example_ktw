f23991879027bfebfd19845119f57a1f9764f4b3
syh8088 <syh8088@gmail.com>
2024-09-11 21:41:53 +0900

    7. 조합수(메모이제이션)
    7. 조합수(메모이제이션)

---
+++ java_algorithms/src/BASE/section9/part7/Main.java
@@ -0,0 +1,39 @@
+package BASE.section9.part7;
+
+import java.util.Scanner;
+
+class Main {
+
+    int[][] dy = new int[35][35];
+
+    public int DFS2(int n, int r) {
+
+
+        return 0;
+    }
+
+
+
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+//        int n = kb.nextInt();
+//        int r = kb.nextInt();
+
+        int n = 5;
+        int r = 3;
+
+        System.out.println(T.DFS(n, r));
+    }
+
+    public int DFS(int n, int r) {
+
+        if (dy[n][r] > 0) return dy[n][r];
+
+        if (n == r || r == 0) return 1;
+        else {
+            return dy[n][r] = DFS(n - 1, r -1) + DFS(n - 1, r);
+        }
+    }
+
+}
