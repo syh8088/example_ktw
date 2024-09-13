5e0b53f29d1f0cdfe20005e77dadcf64e1743907
syh8088 <syh8088@gmail.com>
2024-09-13 23:03:44 +0900

    3. "서로 다른 빈도수 만들기"
    3. "서로 다른 빈도수 만들기"

--- java_algorithms/src/COMPANY_EMULSION/section3/part3/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part3/Solution.java
@@ -1,10 +1,43 @@
 package COMPANY_EMULSION.section3.part3;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution2(String s) {
+
+        int answer = 0;
 
 
-    public static void main(String[] args) {
 
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution("aaabbbcc"));
+        System.out.println(T.solution("aaabbc"));
+        System.out.println(T.solution("aebbbbc"));
+        System.out.println(T.solution("aaabbbcccde"));
+        System.out.println(T.solution("aaabbbcccdddeeeeeff"));
+    }
+
+    public int solution(String s) {
+
+        int answer = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+        return answer;
     }
 }
--- java_algorithms/src/COMPANY_EMULSION/section3/part3/solution/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part3/solution/Solution.java
@@ -1,10 +1,41 @@
 package COMPANY_EMULSION.section3.part3.solution;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution(String s) {
 
+        int answer = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashSet<Integer> ch = new HashSet<>();
+
+        for (char x : s.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        for (char key : map.keySet()) {
+
+            while (ch.contains(map.get(key))) {
+                answer++;
+                map.put(key, map.get(key) - 1);
+            }
+
+            if (map.get(key) == 0) {
+                continue;
+            }
+            ch.add(map.get(key));
+        }
+
+        return answer;
+    }
 
     public static void main(String[] args) {
-
+        Solution T = new Solution();
+        System.out.println(T.solution("aaabbbcc"));
+        System.out.println(T.solution("aaabbc"));
+        System.out.println(T.solution("aebbbbc"));
+        System.out.println(T.solution("aaabbbcccde"));
+        System.out.println(T.solution("aaabbbcccdddeeeeeff"));
     }
 }
