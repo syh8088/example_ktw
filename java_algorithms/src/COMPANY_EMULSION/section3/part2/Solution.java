57d505f9b33e14d704bb34cd98a76666e2491f2e
syh8088 <syh8088@gmail.com>
2024-09-13 21:34:40 +0900

    2. "같은 빈도수 만들기"
    2. "같은 빈도수 만들기"

--- java_algorithms/src/COMPANY_EMULSION/section3/part2/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part2/Solution.java
@@ -1,10 +1,61 @@
 package COMPANY_EMULSION.section3.part2;
 
+import java.util.*;
+
 class Solution {
 
+    public int[] solution2(String s) {
 
+        int[] answer = new int[5];
+
+
+
+        return answer;
+    }
 
     public static void main(String[] args) {
-
+        Solution T = new Solution();
+        System.out.println(Arrays.toString(T.solution("aaabc")));
+        System.out.println(Arrays.toString(T.solution("aabb")));
+        System.out.println(Arrays.toString(T.solution("abcde")));
+        System.out.println(Arrays.toString(T.solution("abcdeabc")));
+        System.out.println(Arrays.toString(T.solution("abbccddee")));
     }
+
+    /**
+     * 내가 풀어보기
+     */
+    public int[] solution(String s) {
+
+        int[] answer = new int[5];
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int maxCount = 0;
+        for (char c : map.keySet()) {
+            Integer i = map.get(c);
+            if (maxCount < i) {
+                maxCount = i;
+            }
+        }
+
+        String str = "abcde";
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            if (map.containsKey(c)) {
+                Integer i1 = map.get(c);
+                answer[i] = maxCount - i1;
+            }
+            else {
+                answer[i] = maxCount;
+            }
+        }
+
+        return answer;
+    }
+
 }
--- java_algorithms/src/COMPANY_EMULSION/section3/part2/solution/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part2/solution/Solution.java
@@ -1,10 +1,38 @@
 package COMPANY_EMULSION.section3.part2.solution;
 
+import java.util.*;
+
 class Solution {
+    public int[] solution(String s) {
 
+        int[] answer = new int[5];
+        HashMap<Character, Integer> map = new HashMap<>();
 
+        for (char x : s.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0)+1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        String tmp = "abcde";
+        for (char key : tmp.toCharArray()) {
+            if (map.getOrDefault(key, 0) > max) {
+                max = map.getOrDefault(key, 0);
+            }
+        }
+
+        for (int i = 0; i < tmp.length(); i++) {
+            answer[i] = max - map.getOrDefault(tmp.charAt(i), 0);
+        }
+
+        return answer;
+    }
 
     public static void main(String[] args) {
-
+        Solution T = new Solution();
+        System.out.println(Arrays.toString(T.solution("aaabc")));
+        System.out.println(Arrays.toString(T.solution("aabb")));
+        System.out.println(Arrays.toString(T.solution("abcde")));
+        System.out.println(Arrays.toString(T.solution("abcdeabc")));
+        System.out.println(Arrays.toString(T.solution("abbccddee")));
     }
 }
