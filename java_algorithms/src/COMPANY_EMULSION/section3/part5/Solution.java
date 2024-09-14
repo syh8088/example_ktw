a6fc374906ec9944457d6ea214b4003adf6ba8bd
syh8088 <syh8088@gmail.com>
2024-09-14 15:21:32 +0900

    5. "회장 선거"
    5. "회장 선거"

--- java_algorithms/src/COMPANY_EMULSION/section3/part5/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part5/Solution.java
@@ -1,10 +1,74 @@
 package COMPANY_EMULSION.section3.part5;
 
+import java.util.*;
+
 class Solution {
 
+    public String solution2(String[] votes, int k) {
+
+        String answer = " ";
 
 
-    public static void main(String[] args) {
+        return answer;
+    }
 
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(new String[]{
+                "john tom",
+                "daniel luis",
+                "john luis",
+                "luis tom",
+                "daniel tom",
+                "luis john"
+        }, 2));
+        System.out.println(T.solution(new String[]{"john tom", "park luis", "john luis", "luis tom", "park tom", "luis john", "luis park", "park john", "john park", "tom john", "tom park", "tom luis"}, 2));
+        System.out.println(T.solution(new String[]{"cody tom", "john tom", "cody luis", "daniel luis", "john luis", "luis tom", "daniel tom", "luis john"}, 2));
+        System.out.println(T.solution(new String[]{"bob tom", "bob park", "park bob", "luis park", "daniel luis", "luis bob", "park luis", "tom bob", "tom luis", "john park", "park john"}, 3));
+    }
+
+    /**
+     * 내가 푼거...... (틀린거...???)??...
+     */
+    public String solution(String[] votes, int k) {
+
+        String answer = " ";
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String vote : votes) {
+            String[] str = vote.split(" ");
+            String s1 = str[0];
+            String s2 = str[1];
+
+            if (!map.containsKey(s2)) {
+                List<String> array = new ArrayList<>();
+                array.add(s1);
+                map.put(s2, array);
+            }
+            else {
+                List<String> orDefault = map.get(s2);
+                orDefault.add(s1);
+            }
+        }
+
+        HashMap<String, Integer> result = new HashMap<>();
+        for (String key : map.keySet()) {
+            List<String> list = map.get(key);
+            if (list.size() >= k) {
+                for (String value : list) {
+                    result.put(value, result.getOrDefault(value, 0) + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (String key : result.keySet()) {
+            int target = result.get(key);
+            if (target > max) {
+                max = target;
+                answer = key;
+            }
+        }
+
+        return answer;
     }
 }
--- java_algorithms/src/COMPANY_EMULSION/section3/part5/solution/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part5/solution/Solution.java
@@ -1,10 +1,63 @@
 package COMPANY_EMULSION.section3.part5.solution;
 
+import java.util.*;
+
 class Solution {
 
+    public String solution(String[] votes, int k) {
+        String answer = " ";
+        HashMap<String, HashSet<String>> voteHash = new HashMap<>();
+        HashMap<String, Integer> candidate = new HashMap<>();
+        HashMap<String, Integer> present = new HashMap<>();
 
+        for (String x : votes) {
+
+            String a = x.split(" ")[0]; // 추천을 한 사람
+            String b = x.split(" ")[1]; // 추천을 받을 사람
+
+            voteHash.putIfAbsent(a, new HashSet<>());
+            voteHash.get(a).add(b);
+            candidate.put(b, candidate.getOrDefault(b, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (String key : voteHash.keySet()) {
+            int cnt = 0;
+            for (String target : voteHash.get(key)) {
+                if (candidate.get(target) >= k) {
+                    cnt++;
+                }
+            }
+
+            present.put(key, cnt);
+            max = Math.max(max, cnt);
+        }
+
+        ArrayList<String> tmp = new ArrayList<>();
+        for (String name : present.keySet()) {
+            if (present.get(name) == max) {
+                tmp.add(name);
+            }
+        }
+
+        tmp.sort((a, b) -> a.compareTo(b));
+        answer = tmp.get(0);
+
+        return answer;
+    }
 
     public static void main(String[] args) {
-
+        Solution T = new Solution();
+        System.out.println(T.solution(new String[] {
+                "john tom",
+                "daniel luis",
+                "john luis",
+                "luis tom",
+                "daniel tom",
+                "luis john"
+        }, 2));
+        System.out.println(T.solution(new String[]{"john tom", "park luis", "john luis", "luis tom", "park tom", "luis john", "luis park", "park john", "john park", "tom john", "tom park", "tom luis"}, 2));
+        System.out.println(T.solution(new String[]{"cody tom", "john tom", "cody luis", "daniel luis", "john luis", "luis tom", "daniel tom", "luis john"}, 2));
+        System.out.println(T.solution(new String[]{"bob tom", "bob park", "park bob", "luis park", "daniel luis", "luis bob", "park luis", "tom bob", "tom luis", "john park", "park john"}, 3));
     }
 }
