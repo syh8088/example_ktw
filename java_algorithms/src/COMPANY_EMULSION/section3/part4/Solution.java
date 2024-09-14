f8f090ec877379786f33b6e987479c63fd19431c
syh8088 <syh8088@gmail.com>
2024-09-14 11:21:29 +0900

    4. "음수가 있는 부분수열"
    4. "음수가 있는 부분수열"

--- java_algorithms/src/COMPANY_EMULSION/section3/part4/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part4/Solution.java
@@ -1,10 +1,51 @@
 package COMPANY_EMULSION.section3.part4;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution2(int[] nums, int m) {
 
+        int answer = 0;
+
+
+
+
+
+
+        return answer;
+    }
 
     public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));
+        System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));
+        System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2}, 3));
+        System.out.println(T.solution(new int[]{-1, 0, 1}, 0));
+        System.out.println(T.solution(new int[]{-1, -1, -1, 1}, 0));
+    }
 
+    public int solution(int[] nums, int m) {
+
+        int answer = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int target = nums[i];
+            sum += target;
+            int i1 = sum - m;
+
+            if (map.containsKey(i1)) {
+                int i2 = map.get(i1);
+                answer += i2;
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return answer;
     }
 }
--- java_algorithms/src/COMPANY_EMULSION/section3/part4/solution/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part4/solution/Solution.java
@@ -1,10 +1,35 @@
 package COMPANY_EMULSION.section3.part4.solution;
 
+import java.util.*;
+
 class Solution {
+    public int solution(int[] nums, int m) {
 
+        int answer = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        map.put(0, 1);
 
-    public static void main(String[] args) {
+        for (int x : nums) {
+            sum += x;
 
+            int target = sum - m;
+            if (map.containsKey(target)) {
+                answer += map.get(target);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));
+        System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));
+        System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2}, 3));
+        System.out.println(T.solution(new int[]{-1, 0, 1}, 0));
+        System.out.println(T.solution(new int[]{-1, -1, -1, 1}, 0));
     }
 }
