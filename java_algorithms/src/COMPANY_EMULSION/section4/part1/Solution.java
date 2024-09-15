ee5ac069a059e22f88a9f117d7f4a666d43f36c0
syh8088 <syh8088@gmail.com>
2024-09-15 10:52:10 +0900

    1. "최대 길이 연속수열"
    1. "최대 길이 연속수열"

--- java_algorithms/src/COMPANY_EMULSION/section4/part1/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section4/part1/Solution.java
@@ -1,10 +1,75 @@
 package COMPANY_EMULSION.section4.part1;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution2(int[] nums) {
 
+        int answer = 0;
 
-    public static void main(String[] args) {
+        return answer;
+    }
 
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
+        System.out.println(T.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
+        System.out.println(T.solution(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
+        System.out.println(T.solution(new int[]{-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1}));
+        System.out.println(T.solution(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
+    }
+    public int solution(int[] nums) {
+
+        int answer = 0;
+        int cnt = 0;
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            hashSet.add(nums[i]);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int target = nums[i];
+            if (!hashSet.contains(target - 1)) {
+                cnt = 0;
+
+                while (hashSet.contains(target)) {
+                    cnt++;
+                    target++;
+                }
+
+                answer = Math.max(cnt, answer);
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * 내가 풀어본거... 틀린거...
+     */
+    public int solution5(int[] nums) {
+
+        int answer = 0;
+        int cnt = 1;
+
+        Arrays.sort(nums);
+        int pre = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int target = nums[i];
+            if (target == pre + 1) {
+                cnt++;
+                answer = Math.max(cnt, answer);
+                pre = target;
+            }
+            else if (target == pre) {
+
+            }
+            else {
+                cnt = 0;
+            }
+        }
+
+        return answer;
     }
 }
