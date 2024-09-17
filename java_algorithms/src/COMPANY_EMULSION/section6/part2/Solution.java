a2a20f1a49d2a0e635b91182280c6729db6679c0
syh8088 <syh8088@gmail.com>
2024-09-17 21:18:31 +0900

    2. "이동 횟수"
    2. "이동 횟수"

--- java_algorithms/src/COMPANY_EMULSION/section6/part2/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section6/part2/Solution.java
@@ -1,10 +1,51 @@
 package COMPANY_EMULSION.section6.part2;
 
+import java.util.*;
 class Solution {
 
+    public int solution2(int[] nums) {
 
+        int answer = 0;
+
+
+
+
+        return answer;
+    }
 
     public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[]{2, 5, 3, 4, 2, 3}));
+        System.out.println(T.solution(new int[]{2, 3, 4, 5}));
+        System.out.println(T.solution(new int[]{3, 3, 3, 3, 3}));
+    }
 
+
+    public int solution(int[] nums) {
+
+        int answer = 0;
+        Arrays.sort(nums);
+
+        int lt = 0;
+        int rt = nums.length - 1;
+        while (lt <= rt) {
+            int ltTarget = nums[lt];
+            int rtTarget = nums[rt];
+            int target = ltTarget + rtTarget;
+
+            if (target <= 5) {
+                lt++;
+                rt--;
+            }
+            else {
+                rt--;
+            }
+
+            answer++;
+        }
+
+
+
+        return answer;
     }
 }
--- java_algorithms/src/COMPANY_EMULSION/section6/part2/solution/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section6/part2/solution/Solution.java
@@ -1,10 +1,37 @@
 package COMPANY_EMULSION.section6.part2.solution;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution(int[] nums) {
 
+        int answer = 0;
+        Arrays.sort(nums);
 
-    public static void main(String[] args) {
+        int left = 0;
+        int right = nums.length - 1;
 
+        while (left <= right) {
+
+            if (nums[left] + nums[right] <= 5) {
+                answer++;
+                left++;
+                right--;
+            }
+            else {
+                answer++;
+                right--;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[]{2, 5, 3, 4, 2, 3}));
+        System.out.println(T.solution(new int[]{2, 3, 4, 5}));
+        System.out.println(T.solution(new int[]{3, 3, 3, 3, 3}));
     }
 }
