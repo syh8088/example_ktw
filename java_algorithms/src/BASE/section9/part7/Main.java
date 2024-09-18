d8af4eaa29820532e173f3c323814e75e3b69b4d
syh8088 <syh8088@gmail.com>
2024-09-18 19:07:32 +0900

    7. 조합수(메모이제이션) - 다시 보기
    7. 조합수(메모이제이션) - 다시 보기

--- java_algorithms/src/BASE/section9/part7/Main.java
+++ java_algorithms/src/BASE/section9/part7/Main.java
@@ -28,11 +28,16 @@ class Main {
 
     public int DFS(int n, int r) {
 
-        if (dy[n][r] > 0) return dy[n][r];
+        if (dy[n][r] > 0) {
+            return dy[n][r];
+        }
 
-        if (n == r || r == 0) return 1;
+        if (n == r || r == 0) {
+            return 1;
+        }
         else {
-            return dy[n][r] = DFS(n - 1, r -1) + DFS(n - 1, r);
+            dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+            return dy[n][r];
         }
     }
 
