586fec73d7765146fab0f3642d09050da1ec67ba
syh8088 <syh8088@gmail.com>
2024-09-18 22:34:26 +0900

    2. "청소" - 다시 풀기
    2. "청소" - 다시 풀기

--- java_algorithms/src/COMPANY_EMULSION/section2/part2/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section2/part2/Solution.java
@@ -1,12 +1,36 @@
 package COMPANY_EMULSION.section2.part2;
 
 import java.util.*;
+
 class Solution {
 
-
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
     public int[] solution2(int[][] board, int k) {
-        int[] answer = new int[2];
 
+        int[] answer = new int[2];
+        int pos = 1;
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < k; i++) {
+
+            int nx = dx[pos];
+            int ny = dy[pos];
+
+            int newX = nx + x;
+            int newY = ny + y;
+
+            if (newX < 0 || newY < 0 || newX >= board.length || newY >= board[0].length || board[newX][newY] == 1) {
+                pos = (pos + 1) % 4;
+                continue;
+            }
+
+            x = newX;
+            y = newY;
+        }
+
+        answer[0] = x;
+        answer[1] = y;
 
         return answer;
     }
@@ -21,7 +45,7 @@ class Solution {
                 {1, 0, 1, 0, 1},
                 {0, 0, 0, 0, 0}
         };
-        System.out.println(Arrays.toString(T.solution(arr1, 10)));
+        System.out.println(Arrays.toString(T.solution2(arr1, 10)));
         int[][] arr2 =
                 {
                     {0, 0, 0, 1, 0, 1},
@@ -31,7 +55,7 @@ class Solution {
                     {0, 0, 0, 0, 0, 0},
                     {0, 0, 0, 0, 0, 0}
                 };
-        System.out.println(Arrays.toString(T.solution(arr2, 20)));
+        System.out.println(Arrays.toString(T.solution2(arr2, 20)));
         int[][] arr3 = {
                 {0, 0, 1, 0, 0},
                 {0, 1, 0, 0, 0},
@@ -39,53 +63,8 @@ class Solution {
                 {1, 0, 0, 0, 1},
                 {0, 0, 0, 0, 0}
         };
-        System.out.println(Arrays.toString(T.solution(arr3, 25)));
+        System.out.println(Arrays.toString(T.solution2(arr3, 25)));
     }
 
-    int[][] dis = new int[][] {
-            {0, 1},
-            {1, 0},
-            {0, -1},
-            {-1, 0}
-    };
 
-    /**
-     * 내가 푼거 틀린거.....
-     */
-    public int[] solution(int[][] board, int k) {
-        int[] answer = new int[2];
-
-        int x = 0;
-        int y = 0;
-
-        int pop = 0;
-        for (int i = 0; i < k; i++) {
-            int[] di;
-            if (
-                    x + dis[pop][0] >= 0
-                            && x + dis[pop][0] < board.length
-                            && y + dis[pop][1] >= 0
-                            && y + dis[pop][1] < board.length
-                            && (board[x + dis[pop][0]][y + dis[pop][1]] == 0)
-            ) {
-
-            }
-            else {
-                i++;
-                if (pop == 3) {
-                    pop = 0;
-                }
-                else {
-                    pop++;
-                }
-            }
-            di = dis[pop];
-            x = x + di[0];
-            y = y + di[1];
-
-//            board[x][y] = 1;
-        }
-
-        return new int[] {x, y};
-    }
 }
--- java_algorithms/src/COMPANY_EMULSION/section2/part2/solution/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section2/part2/solution/Solution.java
@@ -6,28 +6,14 @@ class Solution {
 
         int[] answer = new int[2];
 
-        int n = board.length;
 
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
 
-        int x = 0, y = 0, d = 1, count = 0;
-        while (count < k) {
 
-            count++;
 
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n || board[nx][ny] == 1) {
-                d = (d + 1) % 4;
-                continue;
-            }
-            x = nx;
-            y = ny;
-        }
-        answer[0] = x;
-        answer[1] = y;
 
+
+
+        
         return answer;
     }
 
