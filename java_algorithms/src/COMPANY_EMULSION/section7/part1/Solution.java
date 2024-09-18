b1a44f3ddddef4b38acdd3592e83a89f007abb42
syh8088 <syh8088@gmail.com>
2024-09-18 15:37:22 +0900

    1. "가장 가까운 큰수"
    1. "가장 가까운 큰수"

--- java_algorithms/src/COMPANY_EMULSION/section7/part1/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section7/part1/Solution.java
@@ -1,10 +1,32 @@
 package COMPANY_EMULSION.section7.part1;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution(int n) {
 
+        int answer = 0;
+
+        
+
+
+
+
+        return answer;
+    }
+
+    public void DFS(int L, char[] pm, char[] charArray) {
+
+
+    }
 
     public static void main(String[] args) {
-
+        Solution T = new Solution();
+        System.out.println(T.solution(123));
+        System.out.println(T.solution(321));
+        System.out.println(T.solution(20573));
+        System.out.println(T.solution(27711));
+        System.out.println(T.solution(54312));
     }
 }
