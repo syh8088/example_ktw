8b34290bcf3836b5c4d03146163303fe0956e9d5
syh8088 <syh8088@gmail.com>
2024-09-19 15:37:27 +0900

    송아지를 잡자
    송아지를 잡자

--- java_algorithms/src/COMPANY_EMULSION/section8/part3/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section8/part3/Solution.java
@@ -1,10 +1,45 @@
 package COMPANY_EMULSION.section8.part3;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution2(int s, int e) {
 
+        int answer = 0;
+
+
+
+
+        return answer;
+    }
 
     public static void main(String[] args) {
 
+        Solution T = new Solution();
+        System.out.println(T.solution(1, 11));
+        System.out.println(T.solution(10, 3));
+        System.out.println(T.solution(1, 34567));
+        System.out.println(T.solution(5, 6));
+        System.out.println(T.solution(2, 54321));
+    }
+
+    int[][] df = new int[][] {
+            {1, 0},
+            {-1, 0},
+            {2, 1},
+    };
+
+    public int solution(int s, int e) {
+
+        int answer = 0;
+
+
+
+
+
+
+        
+        return answer;
     }
 }
--- java_algorithms/src/COMPANY_EMULSION/section8/part3/solution/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section8/part3/solution/Solution.java
@@ -1,10 +1,53 @@
 package COMPANY_EMULSION.section8.part3.solution;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution(int s, int e) {
 
+        int[][] ch = new int[2][200001];
+        Queue<Integer> queue = new LinkedList<>();
+        ch[0][s] = 1;
+        queue.offer(s);
+        int L = 0;
+
+        while (!queue.isEmpty()) {
+
+            int len = queue.size();
+            L++;
+
+            for (int i = 0; i < len; i++) {
+
+                int target = queue.poll();
+                for (int nx : new int[]{ target - 1, target + 1, target * 2}) {
+                    if (nx >= 0 && nx <= 200000 && ch[L % 2][nx] == 0) {
+                        ch[L % 2][nx] = 1;
+                        queue.offer(nx);
+                    }
+                }
+            }
+
+            e = e + L;
+
+            if (e > 200000) {
+                return -1;
+            }
+
+            if (ch[L % 2][e] == 1) {
+                return L;
+            }
+        }
+
+        return -1;
+    }
 
     public static void main(String[] args) {
-
+        Solution T = new Solution();
+        System.out.println(T.solution(1, 11));
+        System.out.println(T.solution(10, 3));
+        System.out.println(T.solution(1, 34567));
+        System.out.println(T.solution(5, 6));
+        System.out.println(T.solution(2, 54321));
     }
 }
