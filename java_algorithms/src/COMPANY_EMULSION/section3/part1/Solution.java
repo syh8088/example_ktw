dd37dc8af8299fb6502ffe3c14ce01cfb2487faf
syh8088 <syh8088@gmail.com>
2024-09-20 18:43:09 +0900

    한 번 사용한 최초문자 - 다시 풀기
    한 번 사용한 최초문자 - 다시 풀기

--- java_algorithms/src/COMPANY_EMULSION/section3/part1/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section3/part1/Solution.java
@@ -1,6 +1,5 @@
 package COMPANY_EMULSION.section3.part1;
 
-import java.util.HashMap;
 
 class Solution {
 
@@ -11,37 +10,17 @@ class Solution {
 
 
 
+
+
         return answer;
     }
 
     public static void main(String[] args){
         Solution T = new Solution();
-        System.out.println(T.solution("statitsics"));
-        System.out.println(T.solution("aabb"));
-        System.out.println(T.solution("stringshowtime"));
-        System.out.println(T.solution("abcdeabcdfg"));
+        System.out.println(T.solution2("statitsics"));
+        System.out.println(T.solution2("aabb"));
+        System.out.println(T.solution2("stringshowtime"));
+        System.out.println(T.solution2("abcdeabcdfg"));
     }
 
-    /**
-     * 내가 풀어본거
-     */
-    public int solution(String s) {
-
-        int answer = 0;
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        char[] charArray = s.toCharArray();
-        for (char c : charArray) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : map.keySet()) {
-            if (map.get(c) == 1) {
-                return s.indexOf(c) + 1;
-            }
-        }
-
-        return -1;
-    }
 }
