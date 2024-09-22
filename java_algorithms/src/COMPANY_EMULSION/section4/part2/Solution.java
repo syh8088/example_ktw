f71638c06f6c2c6223c812dc91cc4c1d02b93c62
syh8088 <syh8088@gmail.com>
2024-09-22 11:53:08 +0900

    2. "겹쳐진압축 해제"  - 다시 풀어보기
    2. "겹쳐진압축 해제"  - 다시 풀어보기

--- java_algorithms/src/COMPANY_EMULSION/section4/part2/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section4/part2/Solution.java
@@ -8,17 +8,59 @@ class Solution {
 
         String answer = "";
 
+        Stack<String> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == ')') {
+                String temp = "";
+                while (!stack.isEmpty() && !Character.isDigit(stack.peek().charAt(0))) {
+                    String pop = stack.pop();
+                    if (Character.isAlphabetic(pop.charAt(0))) {
+                        temp =  pop + temp;
+                    }
+                }
+
+                if (stack.isEmpty()) {
+                    stack.push(temp);
+                }
+                else {
+                    String pop = stack.pop();
+                    if (Character.isDigit(pop.charAt(0))) {
+                        String res = "";
+                        int i1 = Integer.parseInt(pop);
+                        for (int i2 = 0; i2 < i1; i2++) {
+                            res += temp;
+                        }
+
+                        if (!res.isEmpty()) {
+                            stack.push(res);
+                        }
+                    }
+                }
+
+
+            }
+            else {
+                stack.push(String.valueOf(c));
+            }
+        }
+
+        for (String x : stack) {
+            answer += x;
+        }
 
         return answer;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        System.out.println(T.solution("3(a2(b))ef"));
-        System.out.println(T.solution("2(ab)k3(bc)"));
-        System.out.println(T.solution("2(ab3((cd)))"));
-        System.out.println(T.solution("2(2(ab)3(2(ac)))"));
-        System.out.println(T.solution("3(ab2(sg))"));
+        System.out.println(T.solution2("3(a2(b))ef"));
+        System.out.println(T.solution2("2(ab)k3(bc)"));
+        System.out.println(T.solution2("2(ab3((cd)))"));
+        System.out.println(T.solution2("2(2(ab)3(2(ac)))"));
+        System.out.println(T.solution2("3(ab2(sg))"));
     }
 
     public String solution(String s) {
