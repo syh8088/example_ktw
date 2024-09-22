0bccde77ee3c3ad722657968772585f06c250276
syh8088 <syh8088@gmail.com>
2024-09-22 10:47:41 +0900

    1. "최대 길이 연속수열" - 다시 풀기
    1. "최대 길이 연속수열" - 다시 풀기

--- java_algorithms/src/COMPANY_EMULSION/section4/part1/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section4/part1/Solution.java
@@ -8,17 +8,25 @@ class Solution {
 
         int answer = 0;
 
+
+        
+
+
+
+
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Solution T = new Solution();
-        System.out.println(T.solution(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
-        System.out.println(T.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
-        System.out.println(T.solution(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
-        System.out.println(T.solution(new int[]{-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1}));
-        System.out.println(T.solution(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
+        System.out.println(T.solution2(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
+        System.out.println(T.solution2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
+        System.out.println(T.solution2(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
+        System.out.println(T.solution2(new int[]{-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1}));
+        System.out.println(T.solution2(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
     }
+
     public int solution(int[] nums) {
 
         int answer = 0;
