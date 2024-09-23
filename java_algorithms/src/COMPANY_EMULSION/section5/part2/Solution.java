69d5876285d10272e49d8b07f1a9c234026dcdf5
syh8088 <syh8088@gmail.com>
2024-09-23 15:48:45 +0900

    2. "수열 찾기" - 다시 풀기
    2. "수열 찾기" - 다시 풀기

--- java_algorithms/src/COMPANY_EMULSION/section5/part2/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section5/part2/Solution.java
@@ -10,14 +10,20 @@ class Solution {
 
 
 
+
+
+
+
+
+        
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Solution T = new Solution();
-        System.out.println(Arrays.toString(T.solution(new int[]{1, 10, 2, 3, 5, 6})));
-        System.out.println(Arrays.toString(T.solution(new int[]{1, 1, 6, 2, 2, 7, 3, 14})));
-        System.out.println(Arrays.toString(T.solution(new int[]{14, 4, 2, 6, 3, 10, 10, 5, 5, 7, 7, 14})));
+        System.out.println(Arrays.toString(T.solution2(new int[]{1, 10, 2, 3, 5, 6})));
+        System.out.println(Arrays.toString(T.solution2(new int[]{1, 1, 6, 2, 2, 7, 3, 14})));
+        System.out.println(Arrays.toString(T.solution2(new int[]{14, 4, 2, 6, 3, 10, 10, 5, 5, 7, 7, 14})));
     }
 
     /**
