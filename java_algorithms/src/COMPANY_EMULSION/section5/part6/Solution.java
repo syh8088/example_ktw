5ca937c921ed03aae4a99fcdf473698edea68f94
syh8088 <syh8088@gmail.com>
2024-09-25 23:53:43 +0900

    멀티태스킹

--- java_algorithms/src/COMPANY_EMULSION/section5/part6/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section5/part6/Solution.java
@@ -1,10 +1,24 @@
 package COMPANY_EMULSION.section5.part6;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution(int[] tasks, long k) {
 
+        int answer = 0;
+
+
+
+
+
+        return answer;
+    }
 
     public static void main(String[] args) {
-
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[]{1, 2, 3}, 5));
+        System.out.println(T.solution(new int[]{8, 5, 2, 9, 10, 7}, 30));
+        System.out.println(T.solution(new int[]{8, 9, 12, 23, 45, 16, 25, 50}, 100));
     }
 }
