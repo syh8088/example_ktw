2ceab1d6c414b0eca7c6c6c9f8ba3795d4e2209c
syh8088 <syh8088@gmail.com>
2024-09-25 16:59:48 +0900

    5. "모임장소"
    5. "모임장소"

--- java_algorithms/src/COMPANY_EMULSION/section5/part5/solution/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section5/part5/solution/Solution.java
@@ -1,10 +1,51 @@
 package COMPANY_EMULSION.section5.part5.solution;
 
+import java.util.*;
+
 class Solution {
 
+    public int solution(int[][] board) {
 
+        int answer = 0;
+        int n = board.length;
+
+        ArrayList<Integer> row = new ArrayList<>();
+        ArrayList<Integer> col = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+
+        col.sort((a, b) -> a - b);
+        int x = row.get(row.size() / 2);
+        int y = col.get(col.size() / 2);
+
+        for (int p : row) {
+            answer += Math.abs(x - p);
+        }
+
+        for (int p : col) {
+            answer += Math.abs(y - p);
+        }
+
+        return answer;
+    }
 
     public static void main(String[] args) {
-
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[][] {
+                {1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0}
+        }));
+        System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
+        System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1}}));
     }
 }
