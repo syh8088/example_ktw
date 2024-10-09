bb8cd77c064b4494d9bf6960f35ef1b8c22e05bb
syh8088 <syh8088@gmail.com>
2024-10-09 14:48:48 +0900

    4. "피부과"  - 다시 풀기
    4. "피부과"  - 다시 풀기

--- java_algorithms/src/COMPANY_EMULSION/section4/part4/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section4/part4/Solution.java
@@ -7,65 +7,36 @@ class Solution {
     public int solution2(int[] laser, String[] enter) {
 
         int answer = 0;
-        int n = enter.length;
-        int[][] inList = new int[n][2];
 
-        for (int i = 0; i < n; i++) {
 
-            String[] split = enter[i].split(" ");
 
-            int a = getTime(split[0]);
-            int b = Integer.parseInt(split[1]);
 
-            inList[i][0] = a;
-            inList[i][1] = b;
-        }
+        
 
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(inList[0][1]);
-        int fT = inList[0][0];
-        int pos = 1;
-
-        for (int t = fT; t <= 1200; t++) {
-            if (pos < n) {
-                int target = inList[pos][0];
-                if (target == t) {
-                    if (queue.isEmpty() && target > fT) {
-                        fT = target;
-                    }
-                    if (target <= fT) {
-                        queue.offer(inList[pos][1]);
-                        pos++;
-                    }
-
-//                    if (queue.isEmpty() && target > fT) {
-//                        fT = target;
-//                    }
-                }
-            }
-
-            if (t == fT && !queue.isEmpty()) {
-                int laserIndex = queue.poll();
-                int i = laser[laserIndex];
-                fT += i;
-            }
-
-            answer = Math.max(answer, queue.size());
-        }
 
         return answer;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        System.out.println(T.solution2(new int[]{30, 20, 25, 15}, new String[]{
+        System.out.println(T.solution2(new int[]{30, 20, 25, 15}, new String[] {
                 "10:23 0", // 623 -> 653
                 "10:40 3", // 640
                 "10:42 2", // 642
                 "10:52 3", // 652
                 "11:10 2"  // 670
         }));
-        System.out.println(T.solution2(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "15:10 0", "15:20 3", "15:22 1", "15:23 0", "15:25 0"}));
+        System.out.println(T.solution2(new int[]{30, 20, 25, 15}, new String[]{
+                "10:23 0", // 623
+                "10:40 3", // 640
+                "10:42 2", // 642
+                "10:52 3", // 652
+                "15:10 0", // 910
+                "15:20 3", // 920
+                "15:22 1", // 922
+                "15:23 0", // 923
+                "15:25 0"  // 925
+        }));
         System.out.println(T.solution2(new int[]{30, 20, 25, 15}, new String[]{"10:20 1", "10:40 1", "11:00 1", "11:20 1", "11:40 1"}));
     }
 
