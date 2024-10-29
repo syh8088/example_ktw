f375e2fc23c9a36feb5dad7f7e3b5a5ba7d0d1f8
syh8088 <syh8088@gmail.com>
2024-10-29 12:15:54 +0900

    가장 가까운 큰수 - 다시 풀어보기
    가장 가까운 큰수 - 다시 풀어보기

--- java_algorithms/src/COMPANY_EMULSION/section7/part1/Solution.java
+++ java_algorithms/src/COMPANY_EMULSION/section7/part1/Solution.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 class Solution {
 
+    int answer;
+
     public int solution(int n) {
 
 
-
-
-
-        return 0;
+        return answer;
     }
 
 
+    
     public static void main(String[] args) {
         Solution T = new Solution();
         System.out.println(T.solution(123));
