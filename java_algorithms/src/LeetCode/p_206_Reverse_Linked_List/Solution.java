c3b6ce3b743d3d49c9692669eb487cf42a7e3d0e
syh8088 <syh8088@gmail.com>
2025-02-19 15:44:46 +0900

    [LeetCode]Reverse Linked List, JAVA 풀이 출처: https://jeonji.tistory.com/27 [만식이의 기록 보관소:티스토리]
    [LeetCode]Reverse Linked List, JAVA 풀이
    출처: https://jeonji.tistory.com/27 [만식이의 기록 보관소:티스토리]

---
+++ java_algorithms/src/LeetCode/p_206_Reverse_Linked_List/Solution.java
@@ -0,0 +1,72 @@
+package LeetCode.p_206_Reverse_Linked_List;
+
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * 💥풀이
+ * https://jeonji.tistory.com/27
+ */
+public class Solution {
+
+
+
+     // Definition for singly-linked list.
+     public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+     }
+
+    public ListNode reverseList2(ListNode head) {
+
+         ListNode prev = null;
+
+
+
+
+
+
+         return prev;
+    }
+
+    public static void main(String[] args) {
+
+        Solution T = new Solution();
+
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
+
+        System.out.println(T.reverseList2(listNode));
+    }
+
+
+
+    /**
+     * 정답
+     */
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode current = head;
+            head = head.next;
+            current.next = prev;
+            prev = current;
+        }
+
+
+        return prev;
+    }
+
+}
